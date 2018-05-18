3fb543240314957ac38b87c47cbb41c38f3529d9
dularibhatt <39335068+dularibhatt@users.noreply.github.com>
2018-05-18 13:39:55 +0530

    Update Scheduler.java

--- src/main/java/myapp/Scheduler.java
+++ src/main/java/myapp/Scheduler.java
@@ -26,6 +26,6 @@ public class DemoServlet extends HttpServlet {
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
     resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"Sample data\" }");
+    resp.getWriter().println("{ \"name\": \"Sample\" }");
   }
 }
